1c8d3044a11dac36cb40e059d55c0f487a7a2f4d
bexact <200103017@stu.sdu.edu.kz>
2022-11-03 00:20:31 +0600

    all activities to fragment

--- app/src/main/java/com/example/projectproductivity/MainActivity.kt
+++ app/src/main/java/com/example/projectproductivity/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.projectproductivity
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.core.view.get
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.projectproductivity.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_intro)
+        binding.imageView2.setOnClickListener {
+            startActivity(Intent(this, ProfileActivity::class.java))
+        }
+        binding.fab.setOnClickListener{
+            startActivity(Intent(this, Todo::class.java))
+        }
+
+
     }
+
 }
