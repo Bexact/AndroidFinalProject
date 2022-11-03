282b0a20c978157f9c265c2cc137eea6e3c845bb
bexact <200103017@stu.sdu.edu.kz>
2022-11-03 10:14:07 +0600

    run with bottom nav

--- app/src/main/java/com/example/projectproductivity/IntroActivity.kt
+++ app/src/main/java/com/example/projectproductivity/IntroActivity.kt
@@ -1,11 +1,19 @@
 package com.example.projectproductivity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projectproductivity.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+    lateinit var binding: ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_intro)
+        binding.buttonsignin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/projectproductivity/MainActivity.kt
+++ app/src/main/java/com/example/projectproductivity/MainActivity.kt
@@ -1,33 +1,26 @@
 package com.example.projectproductivity
 
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import androidx.core.view.get
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.projectproductivity.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_intro)
-        binding.imageView2.setOnClickListener {
-            startActivity(Intent(this, ProfileActivity::class.java))
-        }
-        binding.fab.setOnClickListener{
-            startActivity(Intent(this, Todo::class.java))
-        }
+        setContentView(binding.root)
 
+        val bottomNavigation = binding.bottomNavigationView
 
+        val navController =
+            (supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment)
+                .navController
+
+        bottomNavigation.setupWithNavController(navController)
     }
-
 }
