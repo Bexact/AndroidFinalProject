4d4ec6a66eb58d03a21718236778c34b4e0dd89b
bexact <200103017@stu.sdu.edu.kz>
2022-11-22 23:30:18 +0600

    Reading tasks

--- app/src/main/java/com/example/projectproductivity/TodoFragment.kt
+++ app/src/main/java/com/example/projectproductivity/TodoFragment.kt
@@ -13,12 +13,22 @@ import com.example.projectproductivity.viewmodel.TaskViewModel
 class TodoFragment : Fragment() {
 
     private val viewModel: TaskViewModel by viewModels()
+    private lateinit var adapter: TaskAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentTodoBinding.inflate(inflater)
 
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        adapter = TaskAdapter()
+
+        viewModel.getAllTasks.observe(viewLifecycleOwner){
+            adapter.submitList(it)
+        }
+        binding.tasksRecyclerView.adapter = adapter
         binding.fab2.setOnClickListener {
             findNavController().navigate(R.id.action_todoFragment_to_addFragment)
         }
