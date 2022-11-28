3abf9c8e16201d1920fbe7113bb5f679a9a4b262
Iliyas <ilyas.baygozy@mail.ru>
2022-11-28 14:39:21 +0600

    Firebase for auth

--- app/src/main/java/com/example/projectproductivity/LoginActivity.kt
+++ app/src/main/java/com/example/projectproductivity/LoginActivity.kt
@@ -2,24 +2,51 @@ package com.example.projectproductivity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projectproductivity.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity(){
     lateinit var binding: ActivityLoginBinding
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mAuth = FirebaseAuth.getInstance()
 
         binding.textViewSignUp.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
         binding.btnlogin.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            val email = binding.inputEmail.text.toString()
+            val password = binding.inputPassword.text.toString()
+
+            login(email, password)
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
         }
 
     }
+
+
+    private fun login(email: String, password: String){
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    Toast.makeText(this, "User does not exist", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+
 }
--- app/src/main/java/com/example/projectproductivity/RegisterActivity.kt
+++ app/src/main/java/com/example/projectproductivity/RegisterActivity.kt
@@ -2,20 +2,51 @@ package com.example.projectproductivity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projectproductivity.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity(){
     lateinit var binding: ActivityRegisterBinding
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mAuth = FirebaseAuth.getInstance()
 
         binding.alreadyHaveAccount.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
 
+
+        binding.btnRegister.setOnClickListener{
+            val email = binding.inputEmail.text.toString()
+            val password = binding.inputPassword.text.toString()
+
+            register(email, password)
+        }
     }
+
+
+
+    private fun register(email: String, password: String){
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this,"Some error occured", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
+
+
+
 }
