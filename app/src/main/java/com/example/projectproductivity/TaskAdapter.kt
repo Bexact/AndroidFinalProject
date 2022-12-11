cfa6fa731caba39197661e63cc9fcdc40a1bf6fc
bexact <200103017@stu.sdu.edu.kz>
2022-12-12 00:24:20 +0600

    Click listener 1

--- app/src/main/java/com/example/projectproductivity/TaskAdapter.kt
+++ app/src/main/java/com/example/projectproductivity/TaskAdapter.kt
@@ -1,34 +1,83 @@
 package com.example.projectproductivity
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projectproductivity.database.TaskEntry
 import com.example.projectproductivity.databinding.RowLayoutBinding
+import com.example.projectproductivity.ui.AddFragment
+import com.google.android.play.core.tasks.Tasks
+import java.security.AccessController.getContext
 
 class TaskAdapter : ListAdapter<TaskEntry, TaskAdapter.ViewHolder>(TaskDiffCallback) {
+
+//    private val TasksList = ArrayList<TaskEntry>()
+//    private val fullList = ArrayList<TaskEntry>()
+    private lateinit var mListener: onItemClickListener
     companion object TaskDiffCallback : DiffUtil.ItemCallback<TaskEntry>(){
         override fun areItemsTheSame(oldItem: TaskEntry, newItem: TaskEntry) = oldItem.id == newItem.id
         override fun areContentsTheSame(oldItem: TaskEntry, newItem: TaskEntry) = oldItem == newItem
     }
-    class ViewHolder(val binding: RowLayoutBinding) : RecyclerView.ViewHolder(binding.root){
+
+    class ViewHolder(val binding: RowLayoutBinding, listener: onItemClickListener) : RecyclerView.ViewHolder(binding.root){
+        init {
+            itemView.setOnClickListener {
+
+                listener.onItemClicked(adapterPosition)
+
+            }
+        }
+
         fun bind(taskEntry: TaskEntry){
             binding.taskEntry = taskEntry
             binding.executePendingBindings()
         }
     }
-
-
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val current = getItem(position)
         holder.bind(current)
+//        val cur1 = TasksList[position]
+//        holder.title.text = cur1.title
+//        holder.title.isSelected = true
+//        holder.priority.text = cur1.priority.toString()
+//        holder.date.text = cur1.timestamp.toString()
+//        holder.date.isSelected = true
+//
+//        holder.tasks_layout.setOnClickListener {
+//            listener.onItemClicked(TasksList[holder.adapterPosition])
+//        }
+//        holder.tasks_layout.setOnLongClickListener {
+//            listener.longItemClicked(TasksList[holder.adapterPosition],holder.tasks_layout)
+//            true
+//        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-       return ViewHolder(RowLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false))
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+       return ViewHolder(RowLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false),mListener)
+
+    }
+
+//    fun updateList(newList: List<TaskEntry>){
+//        fullList.clear()
+//        fullList.addAll(newList)
+//
+//        TasksList.clear()
+//        TasksList.addAll(fullList)
+//        notifyDataSetChanged()
+//
+//    }
+    interface onItemClickListener{
+        fun onItemClicked(position: Int)
+
+    }
+    fun setOnItemClickListener(listener: onItemClickListener){
+        mListener = listener
     }
 }
--- app/src/main/java/com/example/projectproductivity/TodoFragment.kt
+++ app/src/main/java/com/example/projectproductivity/TodoFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.projectproductivity.databinding.FragmentTodoBinding
@@ -25,7 +26,11 @@ class TodoFragment : Fragment() {
         binding.viewModel = viewModel
 
         adapter = TaskAdapter()
-
+        adapter.setOnItemClickListener(object : TaskAdapter.onItemClickListener{
+            override fun onItemClicked(position: Int) {
+                Toast.makeText(context,"Hello",Toast.LENGTH_SHORT).show()
+            }
+        })
         viewModel.getAllTasks.observe(viewLifecycleOwner){
             adapter.submitList(it)
         }
